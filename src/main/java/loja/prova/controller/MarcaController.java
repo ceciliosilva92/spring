235c28c484e8813f9ef6f9aeb9baa463317bf757
ceciliosilva92 <73261377+ceciliosilva92@users.noreply.github.com>
2023-09-14 22:16:23 -0300

    erro
    atualizar marca

--- src/main/java/loja/prova/controller/MarcaController.java
+++ src/main/java/loja/prova/controller/MarcaController.java
@@ -6,10 +6,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,21 +21,22 @@ import loja.prova.model.Marca;
 import loja.prova.repository.MarcaRepository;
 
 @Controller
-
+@RequestMapping("/marca")
 public class MarcaController {
 	
 	@Autowired
     MarcaRepository marcaRepository;
 	
-	@RequestMapping(value="/marca", method=RequestMethod.GET)
-	public ModelAndView getMarca() {
-		ModelAndView mav = new ModelAndView("marca");
-        List<Marca> marca = marcaRepository.findAll();
-        mav.addObject("marcas", marca);
-        return mav;
+	@GetMapping
+	public String getMarca(Model mav) {
+	
+		List<Marca> marca = marcaRepository.findAll();
+        mav.addAttribute("marcas", marca);
+        return "marca/index";
 	}
-	@RequestMapping(value="/marca/{id}", method=RequestMethod.GET)
-	public ModelAndView getMarca1(@PathVariable("id") Long id) {
+	
+	@GetMapping("/{id}")
+	public ModelAndView getMarcaid(@PathVariable("id") Long id) {
 		ModelAndView mav = new ModelAndView("marca1");
         Optional<Marca> marca = marcaRepository.findById(id);
         mav.addObject("nome", marca.get().getNome());
@@ -40,12 +44,12 @@ public class MarcaController {
         return mav;
 	}
 	
-	@RequestMapping(value="/marca", method=RequestMethod.POST)
+	@PostMapping
 	public String postMarca(@Valid Marca marca, BindingResult result, RedirectAttributes atribute) {
 		
 		if(result.hasErrors()) {
 			atribute.addFlashAttribute("msg", "Campo Obrigatorio vacio");
-			return "redirect:/marca";
+			return "redirect:marca/index";
 		}
 		atribute.addFlashAttribute("Sucess", "Cadatrada com Sucesso");
 		
@@ -56,16 +60,22 @@ public class MarcaController {
 		
 		return "redirect:/marca";
 	}
-	@RequestMapping(value = "/marca/{Id}/delete", method =RequestMethod.GET)
+	
+	@GetMapping("/{id}/delete")
 	public String deleteMarca(@PathVariable("Id")long id, RedirectAttributes atribute) {
 		marcaRepository.deleteById(id);
 		atribute.addFlashAttribute("Sucess","Deletado com sucesso");
 		return "redirect:/marca";
 		
 	}
-	@RequestMapping(value="/marca/{id}", method=RequestMethod.POST)
-	public String updateMarca(Marca marca, RedirectAttributes atribute ) {
-		
+	
+	@PutMapping("/{id}")
+	public String putMarca( Marca marca, BindingResult result, RedirectAttributes atribute) {
+		System.out.println("por fin");
+		if (result.hasErrors()) {
+			atribute.addFlashAttribute("error", "Algum campo deve ser obrigatório!");
+			return "redirect: /marca/"+ marca.getID();
+		}
 		Marca marcaBase = marcaRepository.findById(marca.getID()).orElse(null);
 		marcaBase.setNome(marca.getNome());
 		marcaBase.setUpdated_at(LocalDateTime.now());
